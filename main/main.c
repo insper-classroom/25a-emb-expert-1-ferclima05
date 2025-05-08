0019b83440969dd83bfb2f161079d4efabcbcdc2
ferclima05 <fernandocl3@al.insper.edu.br>
2025-05-08 15:40:16 -0300

    Código Lab v1

--- main/main.c
+++ main/main.c
@@ -9,7 +9,7 @@
 #include <task.h>
 #include <queue.h>
 
-
+#define MOTOR_C   14
 #define PAN_GPIO  15
 #define TILT_GPIO 16
 
@@ -19,11 +19,15 @@ typedef struct {
 } Joystick_t;
 
 static QueueHandle_t joystick_queue;
+static QueueHandle_t motorC_queue;
+
 
 // ----- Conversão ADC → duty cycle PWM  -----
 static uint16_t adc_to_pwm(uint16_t adc_val) {
     // adc_val ∈ [0..4095] → pulse ∈ [1000..2000] µs
-    const uint16_t min_us = 1000, max_us = 2000;
+    //const uint16_t min_us = 1000, max_us = 2000;
+    const uint16_t min_us = 550, max_us = 2300;
+
     return min_us + ((uint32_t)(adc_val) * (max_us - min_us) / 4095);
 }
 
@@ -31,11 +35,21 @@ static uint16_t adc_to_pwm(uint16_t adc_val) {
 void joystick_task(void *pvParameters) {
     Joystick_t js;
     while (1) {
+        adc_select_input(0);
         js.x = adc_read();       // canal 0
         adc_select_input(1);
         js.y = adc_read();       // canal 1
-        adc_select_input(0);
         xQueueOverwrite(joystick_queue, &js);
+        vTaskDelay(pdMS_TO_TICKS(20));
+    }
+}
+
+// ----- Task de leitura do potenciometro/LDR -----
+void pot_task(void *pvParameters) {
+    while (1) {
+        adc_select_input(2);
+        int leitura = adc_read();       // canal 2
+        xQueueOverwrite(motorC_queue, &leitura);
         vTaskDelay(pdMS_TO_TICKS(20));
     }
 }
@@ -43,10 +57,13 @@ void joystick_task(void *pvParameters) {
 // ----- Task de controle de servos -----
 void servo_task(void *p) {
     Joystick_t js;
+    int16_t pot;
     const uint slice_pan   = pwm_gpio_to_slice_num(PAN_GPIO);
     const uint chan_pan    = pwm_gpio_to_channel(PAN_GPIO);
     const uint slice_tilt  = pwm_gpio_to_slice_num(TILT_GPIO);
     const uint chan_tilt   = pwm_gpio_to_channel(TILT_GPIO);
+    const uint slice_C  = pwm_gpio_to_slice_num(MOTOR_C);
+    const uint chan_C   = pwm_gpio_to_channel(MOTOR_C);
 
     while (1) {
         if (xQueueReceive(joystick_queue, &js, portMAX_DELAY) == pdTRUE) {
@@ -55,6 +72,13 @@ void servo_task(void *p) {
 
             pwm_set_chan_level(slice_pan,  chan_pan,  pulse_pan);
             pwm_set_chan_level(slice_tilt, chan_tilt, pulse_tilt);
+        }
+        if (xQueueReceive(motorC_queue, &pot, portMAX_DELAY) == pdTRUE) {
+            uint16_t pulse_C  = adc_to_pwm(pot);
+
+            printf("raw X=%4d  → pulse=%4dµs\n", pot, pulse_C);
+
+            pwm_set_chan_level(slice_C,  chan_C,  pulse_C);
         }
         vTaskDelay(pdMS_TO_TICKS(20));
     }
@@ -65,13 +89,14 @@ int main() {
 
     // 1) ADC
     adc_init();
+    adc_gpio_init(28);
     adc_gpio_init(26);
     adc_gpio_init(27);
-    adc_select_input(0);
+    //adc_select_input(0);
 
     // 2) PWM para servos
-    const uint servos[2] = { PAN_GPIO, TILT_GPIO };
-    for (int i = 0; i < 2; i++) {
+    const uint servos[3] = { PAN_GPIO, TILT_GPIO, MOTOR_C };
+    for (int i = 0; i < 3; i++) {
         uint gpio    = servos[i];
         uint slice   = pwm_gpio_to_slice_num(gpio);
         uint channel = pwm_gpio_to_channel(gpio);
@@ -87,6 +112,8 @@ int main() {
 
     // 3) queue e tasks (igual a antes) …
     joystick_queue = xQueueCreate(1, sizeof(Joystick_t));
+    motorC_queue = xQueueCreate(1, sizeof(int16_t));
+    xTaskCreate(pot_task,    "POT", 256, NULL, 1, NULL);
     xTaskCreate(joystick_task, "JOY", 256, NULL, 2, NULL);
     xTaskCreate(servo_task,    "SRV", 256, NULL, 1, NULL);
 
